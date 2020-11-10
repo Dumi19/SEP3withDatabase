803d39558c269067d66ee9b9a52500cc3269623e
Maria-Michail <maria.magdalena.michail@gmail.com>
2020-11-10 13:23:18 +0100

    final version

--- Database/Model/Request.cs
+++ Database/Model/Request.cs
@@ -1,21 +1,12 @@
+using System;
+
 namespace Database.Model
 {
+    [Serializable]
     public class Request {
-        private string type;
-        private object arg;
 
-        public Request(string type, object arg) {
-            this.type = type;
-            this.arg = arg;
-        }
-
-        public string getType() {
-            return type;
-        }
-
-        public object getArg() {
-            return arg;
-        }
+        public string Action{get; set;}
+        public object Arg {get; set;}
     }
 }
 
